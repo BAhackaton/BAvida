b9532f3ffa04a8e07db156013a5db7071b7513ef
Ezequiel Becerra <betzerra@Ezequiels-MacBook-Pro.local>
2012-05-11 10:14:37 -0300

    Initial Commit

---
+++ HackatonBA/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HackatonBA
+//
+//  Created by Ezequiel Becerra on 5/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
